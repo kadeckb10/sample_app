4ce929444b5a3f13724f184ce57e369f1b19fdb9
Kadek Dwi Putra <kadekdwiputra10@gmail.com>
2023-01-09 03:27:45 +0800

    Finish chapter 8 | Basic Login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,11 @@
 class SessionsController < ApplicationController
+  
   def new
   end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user &.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page
       reset_session
       log_in(user)
@@ -17,5 +18,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
