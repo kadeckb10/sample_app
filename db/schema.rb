0fda41e65ab02a59abe75db0bcbc979fa6dfac0e
Kadek Dwi Putra <kadekdwiputra10@gmail.com>
2023-01-11 21:11:46 +0800

    test2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_11_053805) do
+ActiveRecord::Schema.define(version: 2023_01_10_065629) do
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "name"
@@ -20,9 +20,6 @@ ActiveRecord::Schema.define(version: 2023_01_11_053805) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
-    t.string "activation_digest"
-    t.boolean "activated", default: false
-    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
