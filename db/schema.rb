09e78110ce7fe5b505459c2dbda23007758a3adb
Kadek Dwi Putra <kadekdwiputra10@gmail.com>
2023-01-16 02:15:02 +0800

    Chapter 13.1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_11_053805) do
+ActiveRecord::Schema.define(version: 2023_01_15_161540) do
+
+  create_table "microposts", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "name"
@@ -26,4 +34,5 @@ ActiveRecord::Schema.define(version: 2023_01_11_053805) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "microposts", "users"
 end
