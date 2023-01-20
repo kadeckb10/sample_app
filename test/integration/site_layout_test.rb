de586f98afbd86719266fc0d8ea0820a3191689b
dwi <kadekdwiputra10@gmail.com>
2023-01-20 19:50:21 +0800

    Chapter 13

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -31,7 +31,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", edit_user_path(@user)
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
-    assert_select "a[href=?]", signup_path
 
     get users_path
     assert_select "title", full_title("All users")
