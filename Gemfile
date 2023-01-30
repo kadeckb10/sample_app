a0f7c30895056573286b53eb4251a2593238e4d4
dwi <kadekdwiputra10@gmail.com>
2023-01-30 13:54:31 +0800

    Finish Chapter 13

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.3', '>= 6.1.3.2'
+gem 'image_processing', '1.12.2'
+gem 'mini_magick', '4.9.5'
 gem 'active_storage_validations', '0.8.9'
 # Use mysql as the database for Active Record
 gem 'mysql2', '~> 0.5'
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -6,4 +6,9 @@ class Micropost < ApplicationRecord
   validates :content, presence: true, length: { maximum: 140 }
   validates :image, content_type: { in: %w[image/jpeg image/gif image/png], message: "must be a valid image format" },
                     size:         { less_than: 5.megabytes, message: "should be less than 5MB" }
+
+  # Returns a resized image for display.
+  def display_image
+    image.variant(resize_to_limit: [200, 200])
+  end
 end
